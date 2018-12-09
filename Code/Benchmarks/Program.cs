27182eba7760e2fc4e7db73485e1fd3357b87f83
Kenny Pflug <kenny.pflug@live.de>
2018-12-09 15:54:15 +0100

    added MustNotBeNullBenchmark example

---
+++ Code/Benchmarks/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace Benchmarks
+{
+    public static class Program
+    {
+        private static IConfig CreateDefaultConfig() => 
+            DefaultConfig
+               .Instance
+               .With(Job.Clr, Job.Core)
+               .With(MemoryDiagnoser.Default)
+               .With(DisassemblyDiagnoser.Create(DisassemblyDiagnoserConfig.Asm));
+        public static void Main(string[] args) =>
+         BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly)
+                          .Run(args, CreateDefaultConfig());
+    }
+}
